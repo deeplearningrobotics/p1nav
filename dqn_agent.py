dbc778ea00ca672b5227dc6a952dd1c8187f659d
Andreas Pasternak <andreas.pasternak@gmx.ch>
2018-09-01 07:16:46 +0200

    Seems I fixed priority replay.

--- dqn_agent.py
+++ dqn_agent.py
@@ -103,11 +103,11 @@ class Agent():
         Q_expected = self.qnetwork_local(states).gather(1, actions)
 
         updated_priorities = torch.abs(Q_targets-Q_expected.detach())
-        #self.memory.update_priorities(indices, updated_priorities)
+        self.memory.update_priorities(indices, updated_priorities)
 
         # Compute loss
-        #loss = F.mse_loss(weights*Q_expected, weights*Q_targets)
-        loss = F.mse_loss(Q_expected, Q_targets)
+        loss = F.mse_loss(weights*Q_expected, weights*Q_targets)
+        #loss = F.mse_loss(Q_expected, Q_targets)
 
         # Minimize the loss
         self.optimizer.zero_grad()
